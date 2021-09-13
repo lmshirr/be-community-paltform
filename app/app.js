7af7510f0b41a0cca7b43675072d9cdf03305247
haris akbar <harisakbar04@gmail.com>
2021-09-13 10:47:16 +0700

    add exception file and func in app.js

--- app/app.js
+++ app/app.js
@@ -1,17 +1,38 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const apiRoutes = require('./routes/index');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const apiRoutes = require("./routes/index");
+const NotFoundException = require("./utils/httpExceptions/notFoundException");
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`This App is Running on port ` + port))
+app.use("/api", apiRoutes);
 
-app.use('/api', apiRoutes);
+// not found routes
+app.all("*", (_, __, next) => {
+  next(new NotFoundException("Route not found"));
+});
+
+app.use(
+  /**
+   *
+   * @param {{message: string, description: object, statusCode: number, isOperational: boolean}} err
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   * @param {import("express").NextFunction} next
+   */
+  function (err, req, res, next) {
+    if (err.description) {
+      return res.status(err.statusCode || 500).json({ message: err.message, errors: err.description });
+    }
+    return res.status(err.statusCode || 500).json({ message: err.message });
+  }
+);
+
+app.listen(port, () => console.log(`This App is Running on port ` + port));
