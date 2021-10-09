c8a2e56b08cf83cd650840ebb67266684544fb12
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-10-09 20:04:53 +0700

    docs: add swagger api docs for google oauth

---
+++ docs/users/getLoggedInGoogleUser.js
@@ -0,0 +1,12 @@
+/**
+ * @swagger
+ * /users/auth/google/user:
+ *   get:
+ *     description: Get logged in google user
+ *     tags:
+ *       - User
+ *     responses:
+ *       200:
+ *         description: Success get logged in google user
+ *
+ */
---
+++ docs/users/redirectGoogleLogin.js
@@ -0,0 +1,12 @@
+/**
+ * @swagger
+ * /users/auth/google:
+ *   get:
+ *     description: Redirect Google login
+ *     tags:
+ *       - User
+ *     responses:
+ *       200:
+ *         description: Success redirect Google login
+ *
+ */
