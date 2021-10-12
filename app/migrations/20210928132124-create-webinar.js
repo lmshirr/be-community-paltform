cb545406f26d8d739e5668d47571f739c01a6cee
Ryan Dritama <dritama2306@gmail.com>
2021-10-12 06:19:39 +0000

    Upload newer file

---
+++ app/migrations/20210928132124-create-webinar.js
@@ -0,0 +1,73 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Webinars', {
+      pk: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: true,
+        unique: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      timezone: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      start: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      end: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      speaker: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      visibility: {
+        type: Sequelize.ENUM('open', 'closed'),
+        allowNull: false,
+        defaultValue: 'open',
+      },
+      class_id: {
+        type: Sequelize.UUID,
+        references: {model: 'class', key:'id'},
+        onDelete: 'CASCADE',
+        allowNull:false
+      },
+      link: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      filename: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Webinars');
+  }
+};
