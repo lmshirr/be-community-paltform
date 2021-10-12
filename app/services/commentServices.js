bc61767ab8a7c3764ec868dc4e41e02225bb8ffa
haris akbar <harisakbar04@gmail.com>
2021-10-12 09:16:48 +0700

    fix comment file undefined

--- app/services/commentServices.js
+++ app/services/commentServices.js
@@ -26,7 +26,7 @@ const postComment = async (postCommentDto, file) => {
 
   // make link path
   let imageUrl;
-  if (file.filename) {
+  if (file) {
     const path =
       process.env.NODE_ENV === 'production'
         ? process.env.PRODUCTION_URL
