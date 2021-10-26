b342deebd3ddf60acbe7d624a4f9fbb915eece8c
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-10-26 23:17:47 +0700

    docs: add api docs for get assessment by id

---
+++ docs/assessments/getAssessmentById.js
@@ -0,0 +1,71 @@
+/**
+ * @swagger
+ * /communities/{communityId}/classes/{classId}/assessments/{assessmentId}:
+ *   parameters:
+ *    - name: communityId
+ *      in: path
+ *      description: The id of the community
+ *      required: true
+ *      schema:
+ *        type: string
+ *    - name: classId
+ *      in: path
+ *      description: The id of the class
+ *      required: true
+ *      schema:
+ *        type: string
+ *    - name: assessmentId
+ *      in: path
+ *      description: assessment id
+ *      required: true
+ *      schema:
+ *        type: string
+ *   get:
+ *    tags:
+ *      - Assessment
+ *    security:
+ *      - jwtToken: []
+ *    description: Get assessment data by id
+ *    responses:
+ *      '200':
+ *         description: Success
+ *         content:
+ *          application/json:
+ *           schema:
+ *            type: object
+ *           properties:
+ *            success:
+ *             type: boolean
+ *            data:
+ *             type: object
+ *             properties:
+ *              title:
+ *                type: string
+ *              description:
+ *                type: string
+ *              duration:
+ *                type: integer
+ *              questions:
+ *                type: array
+ *                items:
+ *                  type: object
+ *                  properties:
+ *                    description:
+ *                      type: string
+ *                    choiceA:
+ *                      type: string
+ *                    choiceB:
+ *                      type: string
+ *                    choiceC:
+ *                      type: string
+ *                    choiceD:
+ *                      type: string
+ *                    correctAnswer:
+ *                      type: string
+ *                      enum: [A, B, C, D]
+ *      '404':
+ *         description: Community not found / Class not found
+ *      '500':
+ *         description: Internal server error
+ *
+ */
