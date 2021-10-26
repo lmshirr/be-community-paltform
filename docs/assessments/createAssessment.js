056cae254b1fb5fb24a64c2eab939f546381fed0
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-10-26 23:12:21 +0700

    docs: fix unshowed docs in swagger

--- docs/assessments/createAssessment.js
+++ docs/assessments/createAssessment.js
@@ -6,68 +6,69 @@
  *      in: path
  *      description: The id of the community
  *      required: true
- *      type: string
+ *      schema:
+ *        type: string
  *    - name: classId
  *      in: path
  *      description: The id of the class
  *      required: true
- *      type: string
- *
- *  post:
- *   tags:
- *   - Assessment
- *   security:
- *   - jwtToken: []
- *   description: Create a new class assessment
- *   requestBody:
- *      required: true
- *      content:
- *          application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 title:
- *                   type: string
- *                 description:
- *                   type: string
- *                 duration:
- *                   type: integer
- *                 questions:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       description:
- *                         type: string
- *                       choiceA:
- *                         type: string
- *                       choiceB:
- *                         type: string
- *                       choiceC:
- *                         type: string
- *                       choiceD:
- *                         type: string
- *                       correctAnswer:
- *                         type: string
- *                         enum: [A, B, C, D]
- *                     required:
- *                      - description
- *                      - choiceA
- *                      - choiceB
- *                      - choiceC
- *                      - choiceD
- *                      - correctAnswer
- *               required:
- *                - title
- *                - description
- *                - duration
- *                - questions
- *   responses:
- *     '201':
- *        description: Created
- *     '404':
- *        description: Community not found / Class not found
- *     '500':
- *        description: Internal server error
+ *      schema:
+ *        type: string
+ *   post:
+ *    tags:
+ *    - Assessment
+ *    security:
+ *    - jwtToken: []
+ *    description: Create a new class assessment
+ *    requestBody:
+ *       required: true
+ *       content:
+ *           application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  title:
+ *                    type: string
+ *                  description:
+ *                    type: string
+ *                  duration:
+ *                    type: integer
+ *                  questions:
+ *                    type: array
+ *                    items:
+ *                      type: object
+ *                      properties:
+ *                        description:
+ *                          type: string
+ *                        choiceA:
+ *                          type: string
+ *                        choiceB:
+ *                          type: string
+ *                        choiceC:
+ *                          type: string
+ *                        choiceD:
+ *                          type: string
+ *                        correctAnswer:
+ *                          type: string
+ *                          enum: [A, B, C, D]
+ *                      required:
+ *                       - description
+ *                       - choiceA
+ *                       - choiceB
+ *                       - choiceC
+ *                       - choiceD
+ *                       - correctAnswer
+ *                required:
+ *                 - title
+ *                 - description
+ *                 - duration
+ *                 - questions
+ *    responses:
+ *      '201':
+ *         description: Created
+ *      '404':
+ *         description: Community not found / Class not found
+ *      '500':
+ *         description: Internal server error
  *
  */
--- docs/assessments/getAllAssessment.js
+++ docs/assessments/getAllAssessment.js
@@ -14,52 +14,52 @@
  *      required: true
  *      schema:
  *        type: string
- *  get:
- *   tags:
- *   - Assessment
- *   security:
- *   - jwtToken: []
- *   description: Get all class assessment data
- *   responses:
- *     '200':
- *        description: Success
- *        content:
- *         application/json:
- *          schema:
- *           type: object
- *          properties:
- *           success:
- *            type: boolean
- *           data:
+ *   get:
+ *    tags:
+ *      - Assessment
+ *    security:
+ *      - jwtToken: []
+ *    description: Get all class assessment data
+ *    responses:
+ *      '200':
+ *         description: Success
+ *         content:
+ *          application/json:
+ *           schema:
  *            type: object
- *            properties:
- *             title:
- *               type: string
- *             description:
- *               type: string
- *             duration:
- *               type: integer
- *             questions:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   description:
- *                     type: string
- *                   choiceA:
- *                     type: string
- *                   choiceB:
- *                     type: string
- *                   choiceC:
- *                     type: string
- *                   choiceD:
- *                     type: string
- *                   correctAnswer:
- *                     type: string
- *                     enum: [A, B, C, D]
- *     '404':
- *        description: Community not found / Class not found
- *     '500':
- *        description: Internal server error
+ *           properties:
+ *            success:
+ *             type: boolean
+ *            data:
+ *             type: object
+ *             properties:
+ *              title:
+ *                type: string
+ *              description:
+ *                type: string
+ *              duration:
+ *                type: integer
+ *              questions:
+ *                type: array
+ *                items:
+ *                  type: object
+ *                  properties:
+ *                    description:
+ *                      type: string
+ *                    choiceA:
+ *                      type: string
+ *                    choiceB:
+ *                      type: string
+ *                    choiceC:
+ *                      type: string
+ *                    choiceD:
+ *                      type: string
+ *                    correctAnswer:
+ *                      type: string
+ *                      enum: [A, B, C, D]
+ *      '404':
+ *         description: Community not found / Class not found
+ *      '500':
+ *         description: Internal server error
  *
  */
